f190cbc932b1a0101e839a2a882154c6aea9a0b9
Yizhou Wang <yizhouw@nvidia.com>
2019-11-22 16:59:08 -0800

    init commit

---
+++ simple-scatter.py
@@ -0,0 +1,22 @@
+
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+# Create random data with numpy
+import numpy as np
+
+N = 1000
+random_x = np.random.randn(N)
+random_y = np.random.randn(N)
+
+# Create a trace
+trace = go.Scatter(
+    x = random_x,
+    y = random_y,
+    mode = 'markers'
+)
+
+data = [trace]
+
+plotly.offline.plot(data, filename='basic-scatter.html')
